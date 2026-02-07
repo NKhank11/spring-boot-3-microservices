9f3acb19d06bbd5973b28ce37763eb8dd55b3b2d
22120157 <namkhanhto23@gmail.com>
2026-02-07 17:02:48 +0700

    feat(inventory): implement inventory service with MySQL and Flyway

---
+++ inventory-service/src/main/resources/db/migration/V2__add_inventory.sql
@@ -0,0 +1,5 @@
+insert into t_inventory (quantity, sku_code)
+values (100, 'MFYP4X/A'),
+       (100, 'G041C'),
+       (100, 'SM-S931B/DS'),
+       (100, 'PJZ110 ');
---
+++ inventory-service/src/test/java/com/nkhank11/microservices/inventory/TestInventoryServiceApplication.java
@@ -0,0 +1,11 @@
+package com.nkhank11.microservices.inventory;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(InventoryServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
