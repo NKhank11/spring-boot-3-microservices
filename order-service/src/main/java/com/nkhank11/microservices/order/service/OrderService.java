6a3a1daf4c6b12e57c0439f0f2f4a8d905b7d354
22120157 <namkhanhto23@gmail.com>
2026-02-07 17:30:35 +0700

    feat: implement Synchronous Communication between Order Service and Inventory Service using the Spring Cloud OpenFeign library

--- order-service/src/main/java/com/nkhank11/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/nkhank11/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.nkhank11.microservices.order.service;
 
+import com.nkhank11.microservices.order.client.InventoryClient;
 import com.nkhank11.microservices.order.dto.OrderRequest;
 import com.nkhank11.microservices.order.model.Order;
 import com.nkhank11.microservices.order.repository.OrderRepository;
@@ -15,8 +16,13 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
+        boolean isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        if (!isProductInStock) {
+            throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() + " is not in stock");
+        }
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
         order.setPrice(orderRequest.price());
