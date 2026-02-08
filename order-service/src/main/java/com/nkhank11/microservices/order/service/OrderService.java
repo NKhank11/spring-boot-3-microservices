fc6db857d406589537564636cc232da393412e1e
22120157 <namkhanhto23@gmail.com>
2026-02-08 13:57:48 +0700

    feat: add related kafka images and implement order processing logic

--- order-service/src/main/java/com/nkhank11/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/nkhank11/microservices/order/service/OrderService.java
@@ -2,12 +2,17 @@ package com.nkhank11.microservices.order.service;
 
 import com.nkhank11.microservices.order.client.InventoryClient;
 import com.nkhank11.microservices.order.dto.OrderRequest;
+import com.nkhank11.microservices.order.event.OrderPlacedEvent;
 import com.nkhank11.microservices.order.model.Order;
 import com.nkhank11.microservices.order.repository.OrderRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Service
@@ -15,19 +20,28 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
 
+    private static final Logger log = LoggerFactory.getLogger(OrderService.class);
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
-        boolean isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
-        if (!isProductInStock) {
-            throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() + " is not in stock");
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        if (isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price().multiply(BigDecimal.valueOf(orderRequest.quantity())));
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+
+            // Send the message to Kafka Topic
+            var orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(), orderRequest.userDetails().email());
+            log.info("Start- Sending OrderPlacedEvent {} to Kafka Topic", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End- Sending OrderPlacedEvent {} to Kafka Topic", orderPlacedEvent);
+        } else {
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
         }
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setQuantity(orderRequest.quantity());
-        order.setSkuCode(orderRequest.skuCode());
-        orderRepository.save(order);
     }
 }
