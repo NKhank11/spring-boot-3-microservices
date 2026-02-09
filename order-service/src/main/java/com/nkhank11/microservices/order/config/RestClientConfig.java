42e93ff6e5c359171cd29a6b4eda6af4965dc8dc
22120157 <namkhanhto23@gmail.com>
2026-02-09 16:08:51 +0700

    feat: Add Kubernetes deployment configuration and fix service configurations

--- order-service/src/main/java/com/nkhank11/microservices/order/config/RestClientConfig.java
+++ order-service/src/main/java/com/nkhank11/microservices/order/config/RestClientConfig.java
@@ -14,7 +14,7 @@ import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
-    @Value("${inventory.url}")
+    @Value("${inventory.service.url}")
     private String inventoryServiceUrl;
 
     @Bean
